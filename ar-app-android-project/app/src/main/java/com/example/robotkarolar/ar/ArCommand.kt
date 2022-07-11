45e2d04d69f757c7e1a27b6e8478e3590aaca92c
Ben Strobel <benedikt.strobel@tum.de>
2022-07-11 14:26:04 +0200

    Made ArCommand Parcelizable

--- ar-app-android-project/app/src/main/java/com/example/robotkarolar/ar/ArCommand.kt
+++ ar-app-android-project/app/src/main/java/com/example/robotkarolar/ar/ArCommand.kt
@@ -2,7 +2,6 @@ package com.example.robotkarolar.ar
 
 import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 
 fun placeBlock(x: Int, y: Int, h: Int, blockType: BlockType): ArCommand {
     return ArCommand(ArCommandType.PLACEBLOCK, x, y, h, blockType)
@@ -32,11 +31,35 @@ fun end(): ArCommand {
     return ArCommand(ArCommandType.END, null, null, null, null)
 }
 
-@Parcelize
-class ArCommand(var arCommandType: ArCommandType, var xPos: Int?, var yPos: Int?, var hPos: Int?, var block: BlockType?) : Parcelable {
-    var commandType: ArCommandType = arCommandType
-    var x: Int? = xPos
-    var y: Int? = yPos
-    var h: Int? = hPos
-    var blockType: BlockType? = block
+class ArCommand(var commandType: ArCommandType, var x: Int?, var y: Int?, var h: Int?, var blockType: BlockType?) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        ArCommandType.valueOf(parcel.readString()!!),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        BlockType.valueOf(parcel.readString()!!)
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(blockType.toString())
+        parcel.writeValue(x)
+        parcel.writeValue(y)
+        parcel.writeValue(h)
+        parcel.writeString(commandType.toString())
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ArCommand> {
+        override fun createFromParcel(parcel: Parcel): ArCommand {
+            return ArCommand(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ArCommand?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
