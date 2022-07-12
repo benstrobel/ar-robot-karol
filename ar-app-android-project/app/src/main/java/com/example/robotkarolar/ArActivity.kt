06042e0e28860073de819e5cd0e6bcb422be2486
Ben Strobel <benedikt.strobel@tum.de>
2022-07-12 15:00:04 +0200

    Using worldOrigin as node parent instead of sceneView
    Fixed object placement
    Improved performance for existing block check
    Implemented rotateKarol
    Reintroduced debug button

--- ar-app-android-project/app/src/main/java/com/example/robotkarolar/ArActivity.kt
+++ ar-app-android-project/app/src/main/java/com/example/robotkarolar/ArActivity.kt
@@ -1,35 +1,39 @@
 package com.example.robotkarolar
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.robotkarolar.ar.ArCommand
 import com.example.robotkarolar.ar.ArCommandType
 import com.example.robotkarolar.ar.BlockType
-import com.example.robotkarolar.ar.placeBlock
 import com.example.robotkarolar.karollogic.Interpreter
 import com.example.robotkarolar.karollogic.instructions.controlflow.CodeBlock
 import com.example.robotkarolar.karollogic.instructions.statements.Noop
 import com.example.robotkarolar.karollogic.world.World
-import com.google.ar.core.Anchor
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.ar.core.Config
 import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.node.ArModelNode
-import io.github.sceneview.ar.node.EditableTransform
+import io.github.sceneview.ar.node.ArNode
 import io.github.sceneview.ar.node.PlacementMode
 import io.github.sceneview.math.Position
 import io.github.sceneview.math.Rotation
+import io.github.sceneview.math.Scale
 import io.github.sceneview.utils.setFullScreen
-import kotlinx.coroutines.delay
 
 class ArActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var sceneView: ArSceneView
-    //private var arrayCommand: ArrayList<ArCommand>? = null
-    //private var indexInCommands: Int = 0
     private var karolCreated = false
     private lateinit var codeBlock: CodeBlock
     private lateinit var interpreter: Interpreter
-    private lateinit var firstAnchor: Anchor
+    private lateinit var worldOrigin: ArNode
+    private lateinit var placeButton: ExtendedFloatingActionButton
+    private val allModelScale = 0.5f
+    private var karolRotation = 0
+
+    override fun onBackPressed() {
+        finishAr()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,19 +58,40 @@ class ArActivity : AppCompatActivity(R.layout.activity_main) {
 
         sceneView = findViewById(R.id.sceneView)
 
+        placeButton = findViewById<ExtendedFloatingActionButton>(R.id.placeModelButton).apply {
+            setOnClickListener{ debug() }
+        }
+
+        sceneView.planeRenderer.isShadowReceiver = false
+        sceneView.onArSessionCreated = {
+            val config = sceneView.arSession?.config
+            config?.lightEstimationMode = Config.LightEstimationMode.ENVIRONMENTAL_HDR
+            sceneView.arSession?.configure(config)
+        }
+
         sceneView.onArFrame = {
             if(!karolCreated && it.isTrackingPlane) {
-                createKarol(0,0,0)
+                sceneView.planeRenderer.isVisible = false
+                worldOrigin = ArModelNode(PlacementMode.PLANE_HORIZONTAL, instantAnchor = true, followHitPosition = false)
+                sceneView.addChild(worldOrigin)
+                createKarol(0f,0f,0f, worldOrigin)
                 karolCreated = true
             }
         }
     }
 
     private fun debug() {
-        createBlock(1, 0,0, BlockType.GRASS)
+        val karol = worldOrigin.children.first { it.name == "Karol" }
+        when(karolRotation){
+            0 -> {karol.rotation = Rotation(y = 90f); karolRotation = 1}
+            1 -> {karol.rotation = Rotation(y = 180f); karolRotation = 2}
+            2 -> {karol.rotation = Rotation(y = 270f); karolRotation = 3}
+            3 -> {karol.rotation = Rotation(y = 0f); karolRotation = 0}
+        }
+        // createBlock(1, 0,0, BlockType.GRASS, worldOrigin)
     }
 
-    fun runNext(v: View) {
+    fun runNext() {
         var command = interpreter.nextStep()
 
         if (command != null) {
@@ -74,7 +99,7 @@ class ArActivity : AppCompatActivity(R.layout.activity_main) {
         }
     }
 
-    fun runAll(v: View) {
+    fun runAll() {
         var command = interpreter.nextStep()
         while (command != null && command.commandType != ArCommandType.END) {
             executeCommand(command)
@@ -82,14 +107,14 @@ class ArActivity : AppCompatActivity(R.layout.activity_main) {
         }
     }
 
-    fun finishAr(v: View){
+    fun finishAr(){
         this.finish()
     }
 
     private fun executeCommand(command: ArCommand) {
-        val x: Int = if (command.x != null) command.x as Int else 0
-        val y: Int = if (command.y != null) command.y as Int else 0
-        val h: Int = if (command.h != null) command.h as Int else 0
+        val x: Float = if (command.x != null) (command.x as Int).toFloat() else 0f
+        val y: Float = if (command.y != null) (command.y as Int).toFloat() else 0f
+        val h: Float = if (command.h != null) (command.h as Int).toFloat() else 0f
         val blockType: BlockType = if (command.blockType != null) command.blockType as BlockType else BlockType.GRASS
 
 
@@ -103,96 +128,111 @@ class ArActivity : AppCompatActivity(R.layout.activity_main) {
         }
     }
 
-    private fun deleteBlock(x: Int, y: Int, h: Int) {
+    private fun deleteBlock(x: Float, y: Float, h: Float, parent: ArNode = worldOrigin) {
         var blockExists = false
-        sceneView.children.forEach{
+        parent.children.forEach{
             if (it.name == "Block$x$y$h") {
                 blockExists = true
             }
         }
 
         if (blockExists) {
-            val firstWithName = sceneView.children.first { it.name == "Block$x$y$h" }
+            val firstWithName = parent.children.first { it.name == "Block$x$y$h" }
             firstWithName.isVisible = false
-            sceneView.removeChild(firstWithName)
+            parent.removeChild(firstWithName)
         }
     }
 
-    private fun moveKarol(x: Int, y: Int, h: Int) { //TODO: Doesn't rereder yet
-        sceneView.children.first { it.name == "Karol" }.position = Position(x.toFloat() , h.toFloat() , y. toFloat()) //TODO: TRANSFORM POSITION
+    private fun moveKarol(x: Float, y: Float, h: Float, parent: ArNode = worldOrigin) { //TODO: Doesn't rereder yet
+        parent.children.first { it.name == "Karol" }.position = Position(x.toFloat() , h.toFloat() , y. toFloat()) //TODO: TRANSFORM POSITION
     }
 
-    private fun rotateKarol(arCommandType: ArCommandType) { //TODO: Doesn't rereder yet
+    private fun rotateKarol(arCommandType: ArCommandType, parent: ArNode = worldOrigin) {
+        val karol = parent.children.first { it.name == "Karol" }
+
         when (arCommandType) {
 
             ArCommandType.ROTATERIGHT -> {
-                sceneView.children.first { it.name == "Karol" }.rotation = Rotation(y = 180.0f) //TODO: Change rotation due to North, East, West, South
+                when(karolRotation){
+                    0 -> {karol.rotation = Rotation(y = 90f); karolRotation = 3}
+                    1 -> {karol.rotation = Rotation(y = 180f); karolRotation = 0}
+                    2 -> {karol.rotation = Rotation(y = 270f); karolRotation = 1}
+                    3 -> {karol.rotation = Rotation(y = 0f); karolRotation = 2}
+                }
             }
 
             ArCommandType.ROTATELEFT -> {
-                sceneView.children.first { it.name == "Karol" }.rotation = Rotation(y = 180.0f) //TODO: Change rotation due to North, East, West, South
+                when(karolRotation){
+                    0 -> {karol.rotation = Rotation(y = 90f); karolRotation = 1}
+                    1 -> {karol.rotation = Rotation(y = 180f); karolRotation = 2}
+                    2 -> {karol.rotation = Rotation(y = 270f); karolRotation = 3}
+                    3 -> {karol.rotation = Rotation(y = 0f); karolRotation = 0}
+                }
             }
 
             else -> return
         }
     }
 
-    private fun createKarol(x: Int, y: Int, h: Int) {
+    private fun createKarol(x: Float, y: Float, h: Float, parent: ArNode = worldOrigin):ArModelNode? {
         //check if karol exists
+        val context = this
         if (!karolCreated) {
             var karolNode = ArModelNode(
-                context = this,
-                lifecycle = lifecycle,
-                modelFileLocation = "model_steve/steveScaled.glb",
-                autoScale = false,
-                // Place the model origin at horizontal and vertical middle
-                centerOrigin = Position(0.0f, 0.0f , 0.0f)
+                PlacementMode.DISABLED,
+                followHitPosition = false,
+                instantAnchor = true,
             ).apply {
+                loadModelAsync(
+                    context = context,
+                    lifecycle = lifecycle,
+                    glbFileLocation = "model_steve/steveScaled.glb",
+                    autoScale = false,
+                    centerOrigin = Position(0f,0f,0f)
+                )
+                position = Position(x, y, h)
+                rotation = Rotation(x= 0f, y = 0f, z = 0f)
                 name = "Karol"
-                instantAnchor = true
-                position = Position(x.toFloat(), y.toFloat(), h.toFloat())
+                scale = Scale(allModelScale)
             }
-            sceneView.apply {
-                addChild(karolNode)
-                planeRenderer.isVisible = false
-            }
+            parent.addChild(karolNode)
+            return karolNode
         }
+        return null
     }
 
-    private fun createBlock(x: Int, y: Int, h: Int, blockType: BlockType) {
-        var modelString: String
-        when (blockType) {
-            BlockType.GRASS -> modelString = "model_grasblock/gras.glb"
-            BlockType.STONE -> modelString = "model_stoneblock/stone.glb"
-            BlockType.WATER -> modelString = "model_waterblock/water.glb"
+    private fun createBlock(x: Float, y: Float, h: Float, blockType: BlockType, parent: ArNode = worldOrigin):ArModelNode? {
+        val context = this
+        val modelString = when (blockType) {
+            BlockType.GRASS -> "model_grasblock/gras.glb"
+            BlockType.STONE -> "model_stoneblock/stone.glb"
+            BlockType.WATER -> "model_waterblock/water.glb"
         }
 
-        //check if block exists
-        var blockExists = false
-        sceneView.children.forEach{
-            if (it.name == "Block$x$y$h") {
-                blockExists = true
-            }
-        }
-
-        if (!blockExists) {
+        //if no block at this coords exists
+        if (parent.children.firstOrNull() { it.name == "Block$x$y$h" } == null) {
             var blockNode = ArModelNode(
-                context = this,
-                lifecycle = lifecycle,
-                modelFileLocation = modelString,
-                //autoAnimate = true,
-                autoScale = false,
-                // Place the model origin at the bottom center
-                centerOrigin = Position(0.0f, 0.0f , 0.0f)
+                PlacementMode.DISABLED,
+                followHitPosition = false,
+                instantAnchor = true,
             ).apply {
+                loadModelAsync(
+                    context = context,
+                    lifecycle = lifecycle,
+                    glbFileLocation = modelString,
+                    autoScale = false,
+                    centerOrigin = Position(0f,0f,0f)
+                )
                 name = "Block$x$y$h"
-
-                instantAnchor = true
-                position = Position(x.toFloat(), y.toFloat(), h.toFloat())
+                scale = Scale(allModelScale)
+                position = Position(x, y, h)
+                rotation = Rotation(y = 0f)
             }
-            sceneView.apply {
+            parent.apply {
                 addChild(blockNode)
             }
+            return blockNode
         }
+        return null
     }
 }
